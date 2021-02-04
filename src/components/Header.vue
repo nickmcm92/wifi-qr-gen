916ef5206a25639ee7c7fbb43ab0195b31f57867
Nick McMahon <nickmcm92@gmail.com>
2021-02-04 19:05:24 +1100

    Updated GitHub link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
       <h1 class="is-size-1">WiFi QR Code Generator</h1>
     </template>
     <template #end>
-      <b-navbar-item href="#"> Check this project out on GitHub </b-navbar-item>
+      <b-navbar-item href="https://github.com/nickmcm92/wifi-qr-gen"> Check this project out on GitHub </b-navbar-item>
     </template>
   </b-navbar>
 </template>
